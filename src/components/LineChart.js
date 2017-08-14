469c2b3cdd9bba66b7178df8715e9b3fbe75578c
Paco <palmtreerooskee@gmail.com>
2017-08-14 07:10:36 -0400

    Today I learned (1) How to navigate the new D3.js library in more depth, specifically d3.time & d3.time-format modules

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react'
 import '../styles/LineChart.css'
-import { scaleLinear } from 'd3-scale'
+import { scaleLinear, scaleTime } from 'd3-scale'
 import { axisRight, axisBottom, axisTop, axisLeft } from 'd3-axis'
 import { max, extent } from 'd3-array'
+import { timeDay, timeWeek, timeMonth } from 'd3-time'
+import { timeParse } from 'd3-time-format'
+import { line } from 'd3-shape'
 import { select } from 'd3-selection'
 
 class LineChart extends Component {
@@ -32,11 +35,27 @@ class LineChart extends Component {
   createLineChart() {
     const { scatterData } = this.state
     // const
-    const node = this.node
-    const xExtent = extent(scatterData, (d) => d.price)
-    const yExtent = extent(scatterData, (d) => d.date)
-    const xScale = scaleLinear().domain(xExtent).range([0,500])
-    const yScale = scaleLinear().domain(yExtent).range([0,500])
+    const node = this.node,
+          svg = select(node),
+          margin = {top: 20, right: 20, bottom: 30, left: 50},
+          width = svg.attr('width') - margin.left - margin.right,
+          height = svg.attr('height') - margin.top - margin.bottom,
+          g = svg.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+    // const parseTime = timeParse('%d-%b-%y')
+    const parseTime = timeParse('%Y-%m-%d')
+
+    const x = scaleTime().rangeRound([0, width])
+    const y = scaleLinear().rangeRound([height, 0])
+
+    const line = line()
+                  .x(d => x(d.date))
+                  .y(d => y(d.close))
+    // const xExtent = extent(scatterData, (d) => d.price)
+    // const yExtent = extent(scatterData, (d) => d.date)
+    // const yScale = scaleLinear().domain(yExtent).range([500,0]) // inverting these numbers will invert the Y scale of the graph
+    // const xScale = scaleLinear().domain(xExtent).range([0,500])
+
+
 
     select(node)
       .selectAll('circle')
@@ -70,7 +89,7 @@ render() {
     border: '1px black solid'
   }
   return <svg ref={node => this.node = node}
-    width={519} height={519} style={svgStyle}>
+    width={960} height={500} style={svgStyle}>
     </svg>
   }
 }
