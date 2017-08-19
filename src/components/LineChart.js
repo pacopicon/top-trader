5592c036cd65987c5ceb5194be3e166a79cf95f9
Paco <palmtreerooskee@gmail.com>
2017-08-19 06:48:54 -0400

    Today I (1) got chart to display all securities in all time scales

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -26,14 +26,13 @@ class LineChart extends Component {
  }
 
  createLineChart() {
-    const { MSFTdailyData } = this.props
+    const { MSFTdailyData, modifier } = this.props
     const node = this.node,
           svg = select(node),
           margin = {top: 20, right: 20, bottom: 30, left: 50},
           width = +svg.attr('width') - margin.left - margin.right,
           height = +svg.attr('height') - margin.top - margin.bottom,
           g = svg.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
-    const parseTime = timeParse('%Y-%m-%d')
 
     const xScale = scaleTime().rangeRound([0, width])
     const yScale = scaleLinear().rangeRound([height, 0])
@@ -54,41 +53,55 @@ class LineChart extends Component {
       return result;
     }
 
-    console.log("MSFTdailyData", MSFTdailyData);
 
-    var dates = MSFTdailyData['Time Series (Daily)']
     console.log('MSFTdailyData = ', MSFTdailyData);
 
+    console.log(`modifier = ${modifier}`);
+    var dates = modifier == 1 ? MSFTdailyData['Time Series (1min)'] : MSFTdailyData['Time Series (Daily)']
+    var timeString = modifier == 1 ? '%Y-%m-%d %H:%M:%S' : '%Y-%m-%d'
+    var parseTime = timeParse(timeString)
+    var formatTime = timeFormat(timeString)
+
     if (dates == null || typeof dates === 'undefined') {
-      var formatTime = timeFormat('%Y-%m-%d')
       var dateArray = [formatTime(new Date), formatTime(new Date)]
-      var priceArray = [['0', '0', '0', '0', '0'],['0', '0', '0', '0', '0']]
+      var priceArray = [['1', '1', '1', '1', '1'],['1', '1', '1', '1', '1']]
+      console.log(`First phase (dateArray and priceArray setters) fired: dateArray = ${dateArray},  priceArray = ${priceArray}`)
     } else {
       var dateArray = Object.keys(dates)
       var priceArray = this.exposePrices(dates)
+      console.log(`Second phase (dateArray and priceArray setters) fired: dateArray.length = ${dateArray.length},  priceArray.length = ${priceArray.length}`)
+      // console.log(`Object.keys(dates) = ${Object.keys(dates)}.`)
+      // Object.keys(dates).map((key) => {console.log("key = ", key)})
     }
 
-    var closeArray = []
+
+    var dataScope = modifier == 1 ? dateArray.length : modifier
+
+    // console.log(`modifier = ${modifier}.  formatTime(new Date) = ${formatTime(new Date)}.  dataScope = ${dataScope}.  parseTime(dateArray[0]) = ${parseTime(dateArray[0])}`)
+    // console.log(`dateArray = ${dateArray}.  priceArray = ${priceArray}.`)
+
+
+    var datePrice = []
 
     this.parseData = function(dateArray, priceArray, mainIndex) {
 
       const dyad = {
         date: parseTime(dateArray[mainIndex]),
-        price: Number(priceArray[mainIndex][3])
+        price: dates == null ? 0 : Number(priceArray[mainIndex][3])
       }
-      closeArray.push(dyad)
-      return closeArray
+      datePrice.push(dyad)
+      return datePrice
     }
 
-    var dataScope = dateArray.length
+
 
     for (var i=0; i<dataScope; i++) {
-      this.parseData(dateArray, priceArray, i, 1)
+      this.parseData(dateArray, priceArray, i)
     }
-    console.log('closeArray = ', closeArray);
+    console.log('datePrice = ', datePrice);
 
-    xScale.domain(extent(closeArray, d => d.date))
-    yScale.domain(extent(closeArray, d => d.price))
+    xScale.domain(extent(datePrice, d => d.date))
+    yScale.domain(extent(datePrice, d => d.price))
 
     g.append('g')
       .attr('transform', 'translate(0, ' + height + ')')
@@ -107,7 +120,7 @@ class LineChart extends Component {
       .text('price ($)')
 
     g.append('path')
-      .datum(closeArray)
+      .datum(datePrice)
       .attr('fill', 'none')
       .attr('stroke', 'steelblue')
       .attr('stroke-linejoin', 'round')
--- src/components/StockDashboard.js
+++ src/components/StockDashboard.js
@@ -12,11 +12,10 @@ class StockDashboard extends Component {
   constructor() {
     super()
     this.renderLineChart = this.renderLineChart.bind(this)
-    this.switchSecurity = this.switchSecurity.bind(this)
-    this.renderSymbolOptions = this.renderSymbolOptions.bind(this)
-    this.renderFuncOptions = this.renderFuncOptions.bind(this)
+    this.changeChartParams = this.changeChartParams.bind(this)
+    this.renderOptions = this.renderOptions.bind(this)
     this.handleSymbolSelection = this.handleSymbolSelection.bind(this)
-    this.handleFuncSelection = this.handleFuncSelection.bind(this)
+    this.handleTimeScaleSelection = this.handleTimeScaleSelection.bind(this)
     this.callAPI = this.callAPI.bind(this)
     this.state = {
       renderLineChart: true,
@@ -28,11 +27,11 @@ class StockDashboard extends Component {
         close: 0,
         volume: 0,
       }, securities: [
-        '','AAPL','ABT','ABBV','ACN','ACE','ADBE','ADT','AAP','AES','AET','AFL','AMG','A','GAS','ARE','APD','AKAM','AA','AGN','ALXN','ALLE','ADS','ALL','ALTR','MO','AMZN','AEE','AAL','AEP','AXP','AIG','AMT','AMP','ABC','AME','AMGN','APH','APC','ADI','AON','APA','AIV','AMAT','ADM','AIZ','T','ADSK','ADP','AN','AZO','AVGO','AVB','AVY','BHI','BLL','BAC','BK','BCR','BXLT','BAX','BBT','BDX','BBBY','BRK.B','BBY','BLX','HRB','BA','BWA','BXP','BSX','BMY','BRCM','BF.B','CHRW','CA','CVC','COG','CAM','CPB','COF','CAH','HSIC','KMX','CCL','CAT','CBG','CBS','CELG','CNP','CTL','CERN','CF','SCHW','CHK','CVX','CMG','CB','CI','XEC','CINF','CTAS','CSCO','C','CTXS','CLX','CME','CMS','COH','KO','CCE','CTSH','CL','CMCSA','CMA','CSC','CAG','COP','CNX','ED','STZ','GLW','COST','CCI','CSX','CMI','CVS','DHI','DHR','DRI','DVA','DE','DLPH','DAL','XRAY','DVN','DO','DTV','DFS','DISCA','DISCK','DG','DLTR','D','DOV','DOW','DPS','DTE','DD','DUK','DNB','ETFC','EMN','ETN','EBAY','ECL','EIX','EW','EA','EMC','EMR','ENDP','ESV','ETR','EOG','EQT','EFX','EQIX','EQR','ESS','EL','ES','EXC','EXPE','EXPD','ESRX','XOM','FFIV','FB','FAST','FDX','FIS','FITB','FSLR','FE','FISV','FLIR','FLS','FLR','FMC','FTI','F','FOSL','BEN','FCX','FTR','GME','GPS','GRMN','GD','GE','GGP','GIS','GM','GPC','GNW','GILD','GS','GT','GOOGL','GOOG','GWW','HAL','HBI','HOG','HAR','HRS','HIG','HAS','HCA','HCP','HCN','HP','HES','HPQ','HD','HON','HRL','HSP','HST','HCBK','HUM','HBAN','ITW','IR','INTC','ICE','IBM','IP','IPG','IFF','INTU','ISRG','IVZ','IRM','JEC','JBHT','JNJ','JCI','JOY','JPM','JNPR','KSU','K','KEY','GMCR','KMB','KIM','KMI','KLAC','KSS','KRFT','KR','LB','LLL','LH','LRCX','LM','LEG','LEN','LVLT','LUK','LLY','LNC','LLTC','LMT','L','LOW','LYB','MTB','MAC','M','MNK','MRO','MPC','MAR','MMC','MLM','MAS','MA','MAT','MKC','MCD','MCK','MJN','MMV','MDT','MRK','MET','KORS','MCHP','MU','MSFT','MHK','TAP','MDLZ','MON','MNST','MCO','MS','MOS','MSI','MUR','MYL','NDAQ','NOV','NAVI','NTAP','NFLX','NWL','NFX','NEM','NWSA','NEE','NLSN','NKE','NI','NE','NBL','JWN','NSC','NTRS','NOC','NRG','NUE','NVDA','ORLY','OXY','OMC','OKE','ORCL','OI','PCAR','PLL','PH','PDCO','PAYX','PNR','PBCT','POM','PEP','PKI','PRGO','PFE','PCG','PM','PSX','PNW','PXD','PBI','PCL','PNC','RL','PPG','PPL','PX','PCP','PCLN','PFG','PG','PGR','PLD','PRU','PEG','PSA','PHM','PVH','QRVO','PWR','QCOM','DGX','RRC','RTN','O','RHT','REGN','RF','RSG','RAI','RHI','ROK','COL','ROP','ROST','RLD','R','CRM','SNDK','SCG','SLB','SNI','STX','SEE','SRE','SHW','SPG','SWKS','SLG','SJM','SNA','SO','LUV','SWN','SE','STJ','SWK','SPLS','SBUX','HOT','STT','SRCL','SYK','STI','SYMC','SYY','TROW','TGT','TEL','TE','TGNA','THC','TDC','TSO','TXN','TXT','HSY','TRV','TMO','TIF','TWX','TWC','TJX','TMK','TSS','TSCO','RIG','TRIP','FOXA','TSN','TYC','UA','UNP','UNH','UPS','URI','UTX','UHS','UNM','URBN','VFC','VLO','VAR','VTR','VRSN','VZ','VRTX','VIAB','V','VNO','VMC','WMT','WBA','DIS','WM','WAT','ANTM','WFC','WDC','WU','WY','WHR','WFM','WMB','WEC','WYN','WYNN','XEL','XRX','XLNX','XL','XYL','YHOO','YUM','ZBH','ZION','ZTS'
+        'AAPL','ABT','ABBV','ACN','ACE','ADBE','ADT','AAP','AES','AET','AFL','AMG','A','GAS','ARE','APD','AKAM','AA','AGN','ALXN','ALLE','ADS','ALL','ALTR','MO','AMZN','AEE','AAL','AEP','AXP','AIG','AMT','AMP','ABC','AME','AMGN','APH','APC','ADI','AON','APA','AIV','AMAT','ADM','AIZ','T','ADSK','ADP','AN','AZO','AVGO','AVB','AVY','BHI','BLL','BAC','BK','BCR','BXLT','BAX','BBT','BDX','BBBY','BRK.B','BBY','BLX','HRB','BA','BWA','BXP','BSX','BMY','BRCM','BF.B','CHRW','CA','CVC','COG','CAM','CPB','COF','CAH','HSIC','KMX','CCL','CAT','CBG','CBS','CELG','CNP','CTL','CERN','CF','SCHW','CHK','CVX','CMG','CB','CI','XEC','CINF','CTAS','CSCO','C','CTXS','CLX','CME','CMS','COH','KO','CCE','CTSH','CL','CMCSA','CMA','CSC','CAG','COP','CNX','ED','STZ','GLW','COST','CCI','CSX','CMI','CVS','DHI','DHR','DRI','DVA','DE','DLPH','DAL','XRAY','DVN','DO','DTV','DFS','DISCA','DISCK','DG','DLTR','D','DOV','DOW','DPS','DTE','DD','DUK','DNB','ETFC','EMN','ETN','EBAY','ECL','EIX','EW','EA','EMC','EMR','ENDP','ESV','ETR','EOG','EQT','EFX','EQIX','EQR','ESS','EL','ES','EXC','EXPE','EXPD','ESRX','XOM','FFIV','FB','FAST','FDX','FIS','FITB','FSLR','FE','FISV','FLIR','FLS','FLR','FMC','FTI','F','FOSL','BEN','FCX','FTR','GME','GPS','GRMN','GD','GE','GGP','GIS','GM','GPC','GNW','GILD','GS','GT','GOOGL','GOOG','GWW','HAL','HBI','HOG','HAR','HRS','HIG','HAS','HCA','HCP','HCN','HP','HES','HPQ','HD','HON','HRL','HSP','HST','HCBK','HUM','HBAN','ITW','IR','INTC','ICE','IBM','IP','IPG','IFF','INTU','ISRG','IVZ','IRM','JEC','JBHT','JNJ','JCI','JOY','JPM','JNPR','KSU','K','KEY','GMCR','KMB','KIM','KMI','KLAC','KSS','KRFT','KR','LB','LLL','LH','LRCX','LM','LEG','LEN','LVLT','LUK','LLY','LNC','LLTC','LMT','L','LOW','LYB','MTB','MAC','M','MNK','MRO','MPC','MAR','MMC','MLM','MAS','MA','MAT','MKC','MCD','MCK','MJN','MMV','MDT','MRK','MET','KORS','MCHP','MU','MSFT','MHK','TAP','MDLZ','MON','MNST','MCO','MS','MOS','MSI','MUR','MYL','NDAQ','NOV','NAVI','NTAP','NFLX','NWL','NFX','NEM','NWSA','NEE','NLSN','NKE','NI','NE','NBL','JWN','NSC','NTRS','NOC','NRG','NUE','NVDA','ORLY','OXY','OMC','OKE','ORCL','OI','PCAR','PLL','PH','PDCO','PAYX','PNR','PBCT','POM','PEP','PKI','PRGO','PFE','PCG','PM','PSX','PNW','PXD','PBI','PCL','PNC','RL','PPG','PPL','PX','PCP','PCLN','PFG','PG','PGR','PLD','PRU','PEG','PSA','PHM','PVH','QRVO','PWR','QCOM','DGX','RRC','RTN','O','RHT','REGN','RF','RSG','RAI','RHI','ROK','COL','ROP','ROST','RLD','R','CRM','SNDK','SCG','SLB','SNI','STX','SEE','SRE','SHW','SPG','SWKS','SLG','SJM','SNA','SO','LUV','SWN','SE','STJ','SWK','SPLS','SBUX','HOT','STT','SRCL','SYK','STI','SYMC','SYY','TROW','TGT','TEL','TE','TGNA','THC','TDC','TSO','TXN','TXT','HSY','TRV','TMO','TIF','TWX','TWC','TJX','TMK','TSS','TSCO','RIG','TRIP','FOXA','TSN','TYC','UA','UNP','UNH','UPS','URI','UTX','UHS','UNM','URBN','VFC','VLO','VAR','VTR','VRSN','VZ','VRTX','VIAB','V','VNO','VMC','WMT','WBA','DIS','WM','WAT','ANTM','WFC','WDC','WU','WY','WHR','WFM','WMB','WEC','WYN','WYNN','XEL','XRX','XLNX','XL','XYL','YHOO','YUM','ZBH','ZION','ZTS'
       ],
-      security: '',
-      funcs: ['1D', '1W', '1M', '3M', '6M', '1Y', '2Y'],
-      func: '',
+      security: 'AAPL',
+      timeScales: ['1D', '1W', '1M', '3M', '6M', '1Y', '2Y'],
+      timeScale: '1D',
       modifier: 1,
       MSFTdailyData: { "Meta Data": {},"Time Series (Daily)": {} }
     }
@@ -40,11 +39,9 @@ class StockDashboard extends Component {
 
   callAPI() {
 
-    const { func } = this.state
-
     var call = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${this.state.security}&outputsize=full&apikey=5JSEEXSISXT9VKNO`
 
-    switch (func) {
+    switch (this.state.timeScale) {
       case '1D':
         this.setState({
           modifier: 1
@@ -84,14 +81,16 @@ class StockDashboard extends Component {
 
     }
 
+
+
     fetch(call)
     .then(response => {
-      console.log('this.state.security = ', this.state.security);
-      console.log('typeof this.state.security = ', typeof this.state.security);
       return response.json()
+      console.log(`this.state.timeScale = ${this.state.timeScale}, this.state.security = ${this.state.security}. call = ${call} `);
     })
     .then(json => {
-      console.log('parsed json: ', json)
+      console.log('parsing succeeded')
+      // console.log('parsed json: ', json)
       // USUAL WAY: this.setState({ microsoft: json })
       // CAN'T DO THIS with a nested state THOUGH.  Need to do this instead:
       // this.setState( { MSFTData.symbol: json['Meta Data']['2. Symbol'] } )
@@ -125,27 +124,28 @@ class StockDashboard extends Component {
   //   this.callAPI() // calling this.callAPI() here and also in componentDidMount() will superinpose all security data on top of each other
   // }
 
-  switchSecurity(choice) {
-    if (!this.state.renderLineChart || this.state.security == '') {
+  changeChartParams(security, timeScale) {
+
+    if (this.state.security !== '' && this.state.timeScale !== '') {
+      console.log(`First phase (changeChartParams) fired: this.state.renderLineChart = ${this.state.renderLineChart},  this.state.security = ${this.state.security}, this.state.timeScale = ${this.state.timeScale}`)
       this.setState({
-        security: choice
+        security: '',
+        timeScale: '',
+        renderLineChart: false
       }, function onceStateIsUpdated() {
-        console.log("first option fired")
-        console.log(`this.state.renderLineChart is ${this.state.renderLineChart} and this.state.security is ${this.state.security}`)
+        this.callAPI()
+        setTimeout(() => this.changeChartParams(security, timeScale), 300)
+      })
+    } else if (this.state.security == '' || this.state.timeScale == '') {
+      this.setState({
+        security: security,
+        timeScale: timeScale
+      }, function onceStateIsUpdated() {
+        console.log(`Second phase (changeChartParams) fired: this.state.renderLineChart = ${this.state.renderLineChart},  this.state.security = ${this.state.security}, this.state.timeScale = ${this.state.timeScale}`)
         this.setState({
           renderLineChart: true
         })
         this.callAPI()
-      })
-    } else if (this.state.renderLineChart && this.state.security !== '') {
-      console.log("second option fired")
-      console.log(`this.state.renderLineChart is ${this.state.renderLineChart} and this.state.security is ${this.state.security}`)
-      this.setState({
-        security: '',
-        renderLineChart: false
-      }, function onceStateIsUpdated() {
-        this.callAPI()
-        setTimeout(() => this.switchSecurity(choice), 300)
       })
     }
   }
@@ -154,39 +154,28 @@ class StockDashboard extends Component {
     return this.state.renderLineChart ? <LineChart className="lineChart"
       MSFTdailyData={this.state.MSFTdailyData}
       security={this.state.security}
-      securities={this.state.securities}
       modifier={this.state.modifier}
     /> : null
   }
 
-  renderSymbolOptions() {
-    const { securities } = this.state,
+  renderOptions(options) {
 
-    secOption = (opt, i) => <option key={i} value={opt}>{opt}</option>
+    const selection = (opt, i) => <option key={i} value={opt}>{opt}</option>
 
-    return securities.map(secOption)
+    return options.map(selection)
 
   }
 
   handleSymbolSelection(event) {
+    const { timeScale } = this.state
     console.log("event.target.value = ", event.target.value);
-    this.switchSecurity(event.target.value)
+    this.changeChartParams(event.target.value, timeScale)
   }
 
-  renderFuncOptions() {
-    const { funcs } = this.state,
-
-    funcOption = (opt, i) => <option key={i} value={opt}>{opt}</option>
-
-    return funcs.map(funcOption)
-
-  }
-
-  handleFuncSelection(event) {
+  handleTimeScaleSelection(event) {
+    const { security } = this.state
     console.log("event.target.value = ", event.target.value);
-    this.setState({
-      func: event.target.value
-    })
+    this.changeChartParams(security, event.target.value)
   }
 
   render() {
@@ -199,10 +188,10 @@ class StockDashboard extends Component {
         {this.renderLineChart()}
         <form>
           <select onChange={this.handleSymbolSelection}>
-            {this.renderSymbolOptions()}
+            {this.renderOptions(this.state.securities)}
           </select>
-          <select onChange={this.handleFuncSelection}>
-            {this.renderFuncOptions()}
+          <select onChange={this.handleTimeScaleSelection}>
+            {this.renderOptions(this.state.timeScales)}
           </select>
         </form>
       </div>
