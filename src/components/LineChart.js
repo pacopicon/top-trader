a150f6e9e3db0e6c493e943873aaf81a41391bce
Paco <palmtreerooskee@gmail.com>
2017-08-25 07:33:40 -0400

    made headway into smoothly-transitioning charts

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+// import React, { PureComponent } from 'react'
 import '../styles/LineChart.css'
 import { scaleLinear, scaleTime } from 'd3-scale'
 import { axisRight, axisBottom, axisTop, axisLeft } from 'd3-axis'
@@ -9,127 +10,181 @@ import { line } from 'd3-shape'
 import { select } from 'd3-selection'
 import { transition } from 'd3-transition'
 
+
+const margin = {top: 20, right: 20, bottom: 80, left: 50},
+  fullWidth = 960,
+  fullHeight = 600,
+  width = fullWidth - margin.left - margin.right,
+  height = fullHeight - margin.top - margin.bottom
+
+const xScale = scaleTime().rangeRound([0, width])
+const yScale = scaleLinear().rangeRound([height, 0])
+
+const lineGenerator = line()
+  .x(d => xScale(d.date))
+  .y(d => yScale(d.price))
+
+// const datePrice = this.props.GRAPHIC
+// xScale.domain(extent(datePrice, d => d.date))
+// yScale.domain(extent(datePrice, d => d.price))
+
+
 class LineChart extends Component {
   constructor(props){
     super(props)
     this.createLineChart = this.createLineChart.bind(this)
     this.rerenderSymbolSelection = this.rerenderSymbolSelection.bind(this)
     this.rerenderTimeScaleSelection = this.rerenderTimeScaleSelection.bind(this)
-    this.transitionLineChart = this.transitionLineChart.bind(this)
- }
+    this.drawAxis = this.drawAxis.bind(this)
+    // this.transitionLineChart = this.transitionLineChart.bind(this)
+  }
 
  componentDidMount() {
-    this.createLineChart()
+    // this.createLineChart()
+    this.drawAxis()
  }
 
  componentDidUpdate() {
-    this.createLineChart()
+    // this.createLineChart()
  }
 
+ // shouldComponentUpdate(this.props.GRAPHIC) {
+ //
+ // }
+
  createLineChart() {
-    const { GRAPHIC, timeScale } = this.props
-    const node = this.node,
-          svg = select(node),
-          margin = {top: 20, right: 20, bottom: 80, left: 50},
-          width = +svg.attr('width') - margin.left - margin.right,
-          height = +svg.attr('height') - margin.top - margin.bottom,
-          g = svg.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
 
-    const xScale = scaleTime().rangeRound([0, width])
-    const yScale = scaleLinear().rangeRound([height, 0])
-    const datePrice = GRAPHIC
+    // const node = this.node,
+    //       svg = select(node),
+    //       margin = {top: 20, right: 20, bottom: 80, left: 50},
+    //       width = +svg.attr('width') - margin.left - margin.right,
+    //       height = +svg.attr('height') - margin.top - margin.bottom,
+          // g = svg.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
 
-    xScale.domain(extent(datePrice, d => d.date))
-    yScale.domain(extent(datePrice, d => d.price))
 
-    console.log(GRAPHIC);
 
-    g.append('g')
-      .attr('transform', 'translate(0, ' + height + ')')
-      .attr('class', 'axisBottom')
-      .call(axisBottom(xScale)
-        .ticks(20)
-        .tickSize(-height)
-      )
-      .selectAll('text')
-        .style("text-anchor", "end")
-        .attr("dx", "-.8em")
-        .attr("dy", ".15em")
-        .attr("transform", "rotate(-65)")
-      .select('.domain')
-      .remove()
 
-    g.append('g')
-      .attr('class', 'axisLeft')
-      .call(axisLeft(yScale)
-        .ticks(20)
-        .tickSize(-width)
-      )
 
-    g.append('path')
-      .datum(datePrice)
-      .attr('fill', 'none')
-      .attr('stroke', '#58B5FD')
-      .attr('stroke-linejoin', 'round')
-      .attr('stroke-width',2)
-      .attr('class', 'line')
-      .attr('d', line().x(d => xScale(d.date)).y(d => yScale(d.price)))
+    // console.log(GRAPHIC);
+    //
+    // g.append('g')
+    //   .attr('transform', 'translate(0, ' + height + ')')
+    //   .attr('class', 'axisBottom')
+    //   .call(axisBottom(xScale)
+    //     .ticks(20)
+    //     .tickSize(-height)
+    //   )
+    //   .selectAll('text')
+    //     .style("text-anchor", "end")
+    //     .attr("dx", "-.8em")
+    //     .attr("dy", ".15em")
+    //     .attr("transform", "rotate(-65)")
+    //   .select('.domain')
+    //   .remove()
+    //
+    // g.append('g')
+    //   .attr('class', 'axisLeft')
+    //   .call(axisLeft(yScale)
+    //     .ticks(20)
+    //     .tickSize(-width)
+    //   )
+    //
+    // g.append('path')
+    //   .datum(datePrice)
+    //   .attr('fill', 'none')
+    //   .attr('stroke', '#58B5FD')
+    //   .attr('stroke-linejoin', 'round')
+    //   .attr('stroke-width',2)
+    //   .attr('class', 'line')
+    //   .attr('d', )
        // for some reason React did not let me isolate the line() function into its own variable even though the   { line } fn was imported from d3.shape.
 
 }
 
-transitionLineChart() {
-  const node = this.node,
-        svg = select(node).transition()
-  var margin = {top: 20, right: 20, bottom: 80, left: 50},
-      width = +svg.attr('width') - margin.left - margin.right,
-      height = +svg.attr('height') - margin.top - margin.bottom
-        // g = svg.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+// transitionLineChart() {
+//   const node = this.node,
+//         svg = select(node).transition()
+//   const margin = {top: 20, right: 20, bottom: 80, left: 50},
+//       width = +svg.attr('width') - margin.left - margin.right,
+//       height = +svg.attr('height') - margin.top - margin.bottom
+//         // g = svg.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+//
+//   const xScale = scaleTime().rangeRound([0, width])
+//   const yScale = scaleLinear().rangeRound([height, 0])
+//   const datePrice = this.props.GRAPHIC
+//
+//   xScale.domain(extent(datePrice, d => d.date))
+//   yScale.domain(extent(datePrice, d => d.price))
+//
+//
+//
+//   svg.select('.line')
+//     .duration(750)
+//     .attr('d', line().x(d => xScale(d.date)).y(d => yScale(d.price)))
+//
+//   svg.select('.axisLeft')
+//     .duration(750)
+//     .call(axisLeft(yScale))
+//
+//   svg.select('.axisBottom')
+//     .duration(750)
+//     .call(axisBottom(xScale))
+//
+// }
 
-  const xScale = scaleTime().rangeRound([0, width])
-  const yScale = scaleLinear().rangeRound([height, 0])
-  const datePrice = this.props.GRAPHIC
 
-  xScale.domain(extent(datePrice, d => d.date))
-  yScale.domain(extent(datePrice, d => d.price))
-
-
-
-  svg.select('.line')
-    .duration(750)
-    .attr('d', line(datePrice).x(d => xScale(d.date)).y(d => yScale(d.price)))
-
-  svg.select('.axisLeft')
-    .duration(750)
-    .call(axisLeft(yScale))
-
-  svg.select('.axisBottom')
-    .duration(750)
-    .call(axisBottom(xScale))
-
+drawAxis() {
+  this.xAxis.call(axisBottom().scale(xScale).ticks(20).tickSize(-height))
+  this.yAxis.call(axisLeft().scale(yScale).ticks(20).tickSize(-width))
 }
+
 
 rerenderSymbolSelection(event) {
   this.props.handleSymbolSelection(event)
-  this.transitionLineChart()
+  // this.transitionLineChart()
 }
 
 rerenderTimeScaleSelection(event) {
   this.props.handleTimeScaleSelection(event)
-  this.transitionLineChart()
+  // this.transitionLineChart()
 }
 
  render() {
+  //  const margin = {top: 20, right: 20, bottom: 80, left: 50},
+  //    fullWidth = 960,
+  //    fullHeight = 600,
+  //    width = fullWidth - margin.left - margin.right,
+  //    height = fullHeight - margin.top - margin.bottom
+   //
+  //  const xScale = scaleTime().rangeRound([0, width])
+  //  const yScale = scaleLinear().rangeRound([height, 0])
+   //
+  //  const lineGenerator = line()
+  //    .x(d => xScale(d.date))
+  //    .y(d => yScale(d.price))
+   //
+   const datePrice = this.props.GRAPHIC
+   xScale.domain(extent(datePrice, d => d.date))
+   yScale.domain(extent(datePrice, d => d.price))
+
     var svgStyle = {
     // borderRight: '1px black solid',
     // borderBottom: '1px black solid'
       border: '1px black solid'
     }
+
     return (
       <div className="lineChart">
-        <svg ref={node => this.node = node}
-          width={960} height={600} style={svgStyle}>
+
+        <svg width='100%' height='100%' viewBox={`0 0 ${fullWidth} ${fullHeight}`} style={svgStyle}>
+          <g transform={`translate(${margin.left},${margin.top})`}>
+            <g className="axis" ref={r => this.xAxis = select(r) } transform={`translate(0, ${height})`}></g>
+            <g className="axis" ref={r => this.yAxis = select(r) }></g>
+            <path className="line" ref={r => this.actual = select(r) } d={lineGenerator(datePrice)}></path>
+          </g>
         </svg>
+
+
         <div className="select">
           <form>
             {
